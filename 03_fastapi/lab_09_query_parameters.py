14216f209f1324f07b6c2d8b9d64514e57bdf544
bharathkumar7733 <chappabharathkumar8@gmail.com>
2026-07-10 00:05:25 +0530

    feat(fastapi): add Lab 09 - Query Parameters

---
+++ 03_fastapi/lab_09_query_parameters.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+books = [
+    {
+        "id": 1,
+        "bookname": "Python",
+        "author": "Guido",
+        "price": 500
+    },
+    {
+        "id": 2,
+        "bookname": "FastAPI",
+        "author": "Sebastian",
+        "price": 600
+    },
+    {
+        "id": 3,
+        "bookname": "Python Advanced",
+        "author": "Guido",
+        "price": 700
+    },
+    {
+        "id": 4,
+        "bookname": "Java",
+        "author": "James",
+        "price": 400
+    }
+]
+filtered_books = []
+
+@app.get("/books")
+def get_books(author: str):
+    filtered_books.clear()
+    for book in books:
+        if book["author"] == author:
+            filtered_books.append(book)
+    return filtered_books
