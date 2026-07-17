1ec7861c2f16615f317e5a89f4862dbe686c86d2
bharathkumar7733 <chappabharathkumar8@gmail.com>
2026-07-17 08:56:58 +0530

    feat(fastapi): add Lab 15 - Validation & Exception Handling

---
+++ 03_fastapi/lab_15_validation_exception.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel, Field
+
+app = FastAPI()
+
+
+class Student(BaseModel):
+    name: str = Field(min_length=2)
+    age: int = Field(gt=17)
+
+
+students = []
+
+
+@app.post("/student")
+def create_student(student: Student):
+    students.append(student)
+    return student
+
+
+@app.get("/student/{student_id}")
+def get_student(student_id: int):
+
+    if student_id < 1 or student_id > len(students):
+        raise HTTPException(
+            status_code=404,
+            detail="Student not found"
+        )
+
+    return students[student_id - 1]
