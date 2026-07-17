814066d1009aef2009343f0c7ef85ea139a6b7a2
bharathkumar7733 <chappabharathkumar8@gmail.com>
2026-07-17 08:56:58 +0530

    feat(fastapi): add Lab 14 - HTTP Status Codes

---
+++ 03_fastapi/lab_14_status_codes.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI, status
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Student(BaseModel):
+    name: str
+    age: int
+
+
+students = []
+
+
+@app.post(
+    "/student",
+    status_code=status.HTTP_201_CREATED
+)
+def create_student(student: Student):
+
+    students.append(student)
+
+    return {
+        "message": "Student created successfully",
+        "student": student
+    }
+
+
+@app.get("/students")
+def get_students():
+
+    return students
